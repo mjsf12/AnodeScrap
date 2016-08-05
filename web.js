63ab65ae9c3d2ed1614f21216916140ff1629504
Marcilio Junior <marciliojr.farias@gmail.com>
2016-08-05 19:40:22 -0300

    jade

--- web.js
+++ web.js
@@ -1,13 +1,52 @@
 var express = require('express');
-var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var jade = require('jade');
 
-app.get('/scrape', function(req, res){
+app.set('view engine', 'jade');
+app.get('/', function(req, res){
+url = 'http://www.animakai.tv/anime/re-zero-kara-hajimeru-isekai-seikatsu';
 
-  //All the web scraping magic will happen here
+request(url, function(error, response, html){
 
+
+      if(!error){
+
+          var $ = cheerio.load(html);
+
+          var title, release, info;
+          var json = { title : "", release : "", info : "", image : ""};
+
+          $('#galeria-animes').filter(function(){
+                        var data = $(this);
+                        title = data.children().children().children().first().text();
+                        title = title.trim();
+                        json.title = title;
+                    })
+          $('.descricao').filter(function(){
+                        var data = $(this);
+                        info = data.text();
+                        info = info.trim();
+                        json.info = info;
+                    })
+          $('.info-geral').filter(function(){
+                          var data = $(this);
+                          release = data.children().eq(4).children().eq(1).text();
+                          release = release.trim();
+                          json.release = release;
+                              })
+          $('.thumb').filter(function(){
+                          var data = $(this);
+                          image = data.children().attr('src');
+                          json.image = image;
+                              })
+
+        console.log(json);
+
+        res.render('index',json)
+      }
+  })
 })
 
 app.listen('8081')
